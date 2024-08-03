248675f01722c8ad6c8c4da1b7d0f5c813a5e7dc
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-08-03 16:53:35 -0300

    finalizando

--- POO/praticando_poo_2/netflix.py
+++ POO/praticando_poo_2/netflix.py
@@ -8,15 +8,33 @@ class Cliente:
         else:
             raise Exception("plano invalido")
     
+
     def mudar_plano(self,novo_plano):
         if novo_plano in self.lista_planos:
             self.plano = novo_plano
         else:
             print("plano invalido")
 
+
+    def ver_filme(self, filme, plano_filme):
+        if self.plano == plano_filme:
+            print(f"ver filme {filme}")
+        elif self.plano == "premium":
+            print(f"ver filme {filme}")
+        elif self.plano == "basic" and plano_filme == "premium":
+            print("faca um upgrada para ver esse filme")
+        else:
+            print("plano invalido")    
+
+
+
+
         
 cliente = Cliente("darlan", "darlan@gmail.com", "basic")
 print(cliente.plano)
+cliente.ver_filme("harry potter", "premium")
+
 
 cliente.mudar_plano("premium")
 print(cliente.plano)
+cliente.ver_filme("harry potter", "premium")
